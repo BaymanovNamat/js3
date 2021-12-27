e7b96974ccbec9cdf3a604a57a531008c852251d
Baymanov Namat <namataxvd@gmail.com>
2021-12-27 15:29:42 +0600

    add html

---
+++ script.js
@@ -0,0 +1,56 @@
+// var year = prompt("what year Kyrgyzstan gained independence?");
+// if (year === "1991") {
+//   alert("correct");
+// }
+// else {
+//   alert("Incorrect");
+// }
+
+// var example1 = "200" + 200;
+// var example2 = "10" + "10";
+// console.log(example2);
+//  var age = prompt("how old are you?"); 
+//  var year = 2020 - age;
+//  console.log(year);
+
+// var year = prompt("when year Kyrgyzstan gained independence?");
+// if (year > "1991") {
+//   alert("This year is after independence year");
+// }
+// else if (year === "1991") {
+//   alert("You got it!");
+// }
+// else {
+//   alert("This year is before independence year");
+// }
+
+// var city = prompt("what city do you live?");
+// if (city === "Karakol") {
+//   alert("you are from Issyk-Kul");
+// }
+// else {
+//   alert("where do you live?");
+// }
+// var currentNumber = 11;
+// var stopNumber = 0;
+// while (currentNumber > stopNumber) {
+//   currentNumber = currentNumber - 1;
+//   console.log(currentNumber);
+// }
+// for (var i=1; i<=10; i++) {
+//   console.log(i);
+// }
+var country = prompt("what is the country you want know capital of?"); 
+if (country === "USA") {
+  alert("Washington");
+}
+else if (country === "Kyrgyzstan") {
+  alert("Bishkek");
+}
+else if (country === "Kazakhstan") {
+  alert("Nur Sultan");
+}
+
+else {
+  alert("we don't know that country");
+}
---
+++ script2.js
@@ -0,0 +1,78 @@
+// var a = 5;
+// var b = "8" ;
+// var d = a + b;
+// alert(d);
+
+
+// var message = prompt("what is your message?");
+// if (message === "") {
+//   alert("no message");
+// }
+// alert(message);
+
+// var year = prompt("when Kyrgyzstan gained independence?");
+//  if (year === "1991") {
+//   alert("correct");
+// }
+//  else {
+//  alert("Incorrect");
+// }
+
+
+// var iAmEighteen = prompt("How old is Namat?");
+// if (iAmEighteen === "18") {
+//   alert("You are right");
+// }
+// else {
+//   alert("you don't know his!");
+// }
+
+// var age = prompt("How old are you?");
+// var year = 2021 - age;
+// alert(year);
+
+// var year = prompt("when year Kyrgyzstan gained independence?"); 
+// if (year > 1991) {
+//   alert("This year is after independence year!");
+// }
+// else if (year === "1991") {
+//   alert("You got it!");
+// }
+// else {
+//   alert("This year is before independence year!");
+// }
+
+// var sister = prompt("how old was her?");
+// if (sister > 58) {
+//   alert("you couldn't find her name!");
+// }
+// else if (sister === "58") {
+//   alert("You know her very well!");
+// }
+// else {
+//   alert("You didn't find!");
+// }
+
+
+
+// var currentNumber = 1;
+// var stopNumber = 6;
+  
+// while (currentNumber < stopNumber) {
+//   currentNumber = currentNumber + 1;
+//   console.log(currentNumber);
+// }
+
+// while (currentNumber < stopNumber) {
+//   stopNumber = stopNumber - 1;
+//   console.log(stopNumber);
+// }
+
+
+// for (var i=1; i<=10; i++) {
+//   console.log(i);
+// }
+
+for (var i=1; i<=10; i--) {
+  console.log(i)
+}
